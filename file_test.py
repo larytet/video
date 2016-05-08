5312546ef744c51708d1afcdbd07aec3d61f9eed
larytet <larytet@yahoo.com>
2016-05-08 11:29:23 +0300

    Test of fast writing of files

--- file_test.py
+++ file_test.py
@@ -1,91 +1,28 @@
-import contextlib
-import errno
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Write to all existing files in the Documents directory  
+
 import os
-import sys
-import tempfile
-if (sys.platform == 'win32'):
-    import win32api, win32con
-
-if sys.platform != 'win32':
-    def _atomic_rename(src, dst):
-        os.rename(src, dst)
-
-    def _atomic_move(src, dst):
-        os.link(src, dst)
-        os.unlink(src)
-else:
-    def _atomic_rename(src, dst):   
-        win32api.MoveFileEx(src, dst, win32con.MOVEFILE_WRITE_THROUGH |  win32con.MOVEFILE_REPLACE_EXISTING)
-
-    def _atomic_move(src, dst):
-        win32api.MoveFileEx(src, dst, win32con.MOVEFILE_WRITE_THROUGH)
-        
-
-def atomic_rename(src, dst):
-    return _atomic_rename(src, dst)
+import thread
+import time
 
 
-def atomic_move(src, dst):
-    return _atomic_move(src, dst)
+log_file_name = os.path.join(".", "write_files.log") 
+log_file = open(log_file_name, "w");
+target_folder = os.path.join(".", "Documents")
+target_files = os.listdir(target_folder)
 
+def write_file(filename):
+    print filename
+    time.sleep(1)
+    log_file.write(filename+",")
+    target_file = open(target_file_name, "w")
+    target_file.write('test')
+    log_file.flush()
 
-class FileAtomic(object):
-    def __init__(self, path, mode='w', overwrite=False):
-        if 'a' in mode:
-            raise ValueError('Appending to a file is not supported')
-        if 'x' in mode:
-            raise ValueError('Use the `overwrite`-parameter instead.')
-        if 'w' not in mode:
-            raise ValueError('file_atomic can only be written to.')
-        self._f = None  
-        self._path = path
-        self._mode = mode
-        self._overwrite = overwrite
+for target_file_name in target_files:
+    thread.start_new_thread(write_file, (target_file_name,) )
+    time.sleep(100)
+    
 
-    def open(self):
-        '''
-        Will open a temporary file.
-        '''
-        success = False
-        try:
-            self._f = self.create_file()
-            success = True
-        finally:
-            if not success:
-                try:
-                    self.cleanup(self._f)
-                except Exception:
-                    pass
-        return self._f
-
-    def create_file(self, dir=None, **kwargs):
-        if dir is None:
-            dir = os.path.dirname(self._path)
-        f = tempfile.NamedTemporaryFile(mode=self._mode, dir=dir,
-                                           delete=False, **kwargs)
-        return f
-
-    def sync(self):
-        '''Flush and sync everything'''
-        self._f.flush()
-        os.fsync(self._f.fileno())
-
-    def commit(self):
-        '''Copy the temporary file to the final location'''
-        if self._overwrite:
-            atomic_rename(self._f.name, self._path)
-        else:
-            atomic_move(self._f.name, self._path)
-       
-    def cleanup(self, f):
-        os.unlink(f.name)
-
-    def close(self):
-        self.commit()
-        self._f.close()
-
-
-fa = FileAtomic("/tmp/t")
-f = fa.open()
-f.write("Hello");
-fa.close()
+    
