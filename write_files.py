c842404062c947d8f422294ed6096bd1c1ed724b
larytet <larytet@yahoo.com>
2016-05-08 21:03:35 +0300

    Test of fast writing of files

--- write_files.py
+++ write_files.py
@@ -7,18 +7,24 @@ def write_file(target_file_name, target_file):
     target_file.write('test')
     print "."
 
-master_file = os.path.join("test.txt") 
+master_file = os.path.join(".", "test.txt") 
 f = open(master_file, "w")
 f.write("test")
 f.close()
+temp_file = os.path.join(".", "test1.txt") 
+f = open(master_file, "w")
+f.write("test1")
+f.close()
+print temp_file
+win32file.ReplaceFile(temp_file, master_file, NULL, 0, NULL, NULL)
 
 opened_files = []
 for target_file_name in os.listdir(os.path.join(".", "Documents")):
-    #try:
-    print target_file_name, master_file
-    win32file.ReplaceFile(target_file_name, master_file, NULL, 0, NULL, NULL)
-    #except Exception as e:
-    #    print e
+    try:
+        print target_file_name
+        win32file.ReplaceFile(target_file_name, master_file, NULL, 0, NULL, NULL)
+    except Exception as e:
+        print e
 time.sleep(10)
 # close all handles, flush the data
 for file_handle in opened_files:
