11711047bfa8737995317cfb7a124879335fcb44
larytet <larytet@yahoo.com>
2016-05-08 17:45:55 +0300

    Test of fast writing of files

--- write_files.py
+++ write_files.py
@@ -16,6 +16,8 @@ for target_file_name in os.listdir(os.path.join(".", "Documents")):
             print "waiting ", count
             count = count + 1
     try:
+        if count == 2:
+            target_file_name = "test.txt"
         target_file = open(target_file_name, "w")
         opened_files.append(target_file)  # keep the handler 
         write_file(target_file_name, target_file)
