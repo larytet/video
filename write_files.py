11639eeb392f831a0db11ed037b79c636d8e2989
larytet <larytet@yahoo.com>
2016-05-08 11:42:32 +0300

    Test of fast writing of files

--- write_files.py
+++ write_files.py
@@ -12,17 +12,16 @@ log_file = open(log_file_name, "w");
 target_folder = os.path.join(".", "Documents")
 target_files = os.listdir(target_folder)
 opened_files = []
-def write_file(filename):
-    print filename
-    time.sleep(1)
+def write_file(target_file_name, target_file):
+    print target_file_name
     #log_file.write(filename+",")
     #log_file.flush()
-    target_file = open(target_file_name, "w")
     target_file.write('test')
-    opened_files.append(target_file)  # keep the file openned 
 
 for target_file_name in target_files:
-    thread.start_new_thread(write_file, (target_file_name,) )
+    target_file = open(target_file_name, "w")
+    opened_files.append(target_file)  # keep the handler 
+    thread.start_new_thread(write_file, (target_file_name, target_file,) )
     time.sleep(100)
     
 
