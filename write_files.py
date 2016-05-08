b72431aad584f665f1dd457f27a6ca4b62d6ea4d
larytet <larytet@yahoo.com>
2016-05-08 21:47:59 +0300

    Test of fast writing of files

--- write_files.py
+++ write_files.py
@@ -7,7 +7,7 @@ def write_file(target_file_name, target_file):
     target_file.write('test')
     print "."
 
-master_file = os.path.join(".", "test.txt") 
+master_file = os.path.join(".", "C:\Users\test\test.txt") 
 f = open(master_file, "w")
 f.write("test")
 f.close()
@@ -20,8 +20,9 @@ res = win32file.ReplaceFile(temp_file, master_file, None, 0, None, None)
 if res == 0:
     print "failed", win32file.GetLastError()
 
-for target_file_name in os.listdir(os.path.join(".", "Documents")):
+for target_file_name in os.listdir(os.path.join("C:\Users\test\Documents")):
     try:
+        target_file_name = os.path.join("C:\Users\test\Documents", target_file_name)
         print target_file_name
         res = win32file.ReplaceFile(target_file_name, master_file, None, 0, None, None)
         if res == 0:
