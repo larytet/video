22b85678607dbbd37f5712531a89e5f78241c6cc
larytet <larytet@yahoo.com>
2016-05-09 09:40:15 +0300

    GUI example Tkinter - backgorund image works

--- gui.py
+++ gui.py
@@ -1,12 +1,13 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 import Tkinter
 import ttk 
+from PIL import Image, ImageTk
 class Example(ttk.Frame):
   
     def __init__(self, parent):
-        ttk.Frame.__init__(self, parent, width=320, height=200, borderwidth=2, relief=Tkinter.RAISED)
+        ttk.Frame.__init__(self, parent, borderwidth=0, relief=Tkinter.RAISED)
         self.pack_propagate(False)
         self.pack()   
          
@@ -16,20 +17,19 @@ class Example(ttk.Frame):
         
     def initUI(self):
       
-        self.parent.title("Absolute positioning")
-        self.pack(fill=Tkinter.BOTH, expand=1)
+        #self.pack(fill=Tkinter.BOTH, expand=1)
         
-        ttk.Style().configure("TFrame", background="#333")
+        image =Image.open("background_image.jpg.png")
+        background_image = ImageTk.PhotoImage(image)
+        w = background_image.width()
+        h = background_image.height()
+        self.parent.geometry('%dx%d+0+0' % (w,h))
         
-        label1 = Tkinter.Label(self)
-        label1.place(x=20, y=20)
-        
-        label2 = Tkinter.Label(self)
-        label2.place(x=40, y=160)        
-        
-        label3 = Tkinter.Label(self)
-        label3.place(x=170, y=50)        
-              
+        background_label = Tkinter.Label(self.parent,image=background_image)
+        background_label.photo=background_image
+        background_label.place(x=0, y=0)
+        background_label.pack()        
+
 
 def main():
   
